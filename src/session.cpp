aa8ac908fb9b56ef30720a80e520d49e8aee7a0a
Kento Nishi <kento24gs@outlook.com>
2024-01-06 08:39:37 -0500

    1 peer at a time

--- src/session.cpp
+++ src/session.cpp
@@ -59,9 +59,9 @@ Session::Session(std::mutex& mtx)
     sp.set_int(sp.out_enc_policy, lt::settings_pack::pe_forced);
     sp.set_int(sp.in_enc_policy, lt::settings_pack::pe_forced);
     sp.set_int(sp.allowed_enc_level, lt::settings_pack::pe_rc4);
-    sp.set_int(sp.max_peerlist_size, 5);
-    sp.set_int(sp.max_paused_peerlist_size, 5);
-    sp.set_int(sp.connections_limit, 5);
+    sp.set_int(sp.max_peerlist_size, 1);
+    sp.set_int(sp.max_paused_peerlist_size, 1);
+    sp.set_int(sp.connections_limit, 1);
 
 #if LIBTORRENT_VERSION_NUM >= 10102
     sp.set_int(sp.urlseed_max_request_bytes, 100 * 1024);
