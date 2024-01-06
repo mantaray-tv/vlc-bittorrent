421a252f1629b52cfd8f1d1a5a3743483c052382
Kento Nishi <kento24gs@outlook.com>
2024-01-06 07:06:59 -0500

    change settings for sandbox

--- src/session.cpp
+++ src/session.cpp
@@ -44,19 +44,22 @@ Session::Session(std::mutex& mtx)
 
     /* Really aggressive settings to optimize time-to-play */
     sp.set_bool(sp.strict_end_game_mode, false);
-    sp.set_bool(sp.announce_to_all_trackers, true);
-    sp.set_bool(sp.announce_to_all_tiers, true);
+    // settings for mantaray.tv
+    // sp.set_bool(sp.announce_to_all_trackers, true);
+    sp.set_bool(sp.announce_to_all_trackers, false);
+    // sp.set_bool(sp.announce_to_all_tiers, true);
+    sp.set_bool(sp.announce_to_all_tiers, false);
     sp.set_int(sp.stop_tracker_timeout, 1);
     sp.set_int(sp.request_timeout, 2);
     sp.set_int(sp.whole_pieces_threshold, 5);
     sp.set_int(sp.request_queue_time, 1);
     sp.set_int(sp.urlseed_pipeline_size, 2);
 
-    // force encryption for mantaray.tv
-    sp.set_int(sp.out_enc_policy, lt::settings_pack::pe_forced);
-    sp.set_int(sp.in_enc_policy, lt::settings_pack::pe_forced);
-    sp.set_int(sp.allowed_enc_level, lt::settings_pack::pe_rc4);
-    sp.set_int(sp.max_peerlist_size, 10);
+    // settings for mantaray.tv
+    // sp.set_int(sp.out_enc_policy, lt::settings_pack::pe_forced);
+    // sp.set_int(sp.in_enc_policy, lt::settings_pack::pe_forced);
+    // sp.set_int(sp.allowed_enc_level, lt::settings_pack::pe_rc4);
+    // sp.set_int(sp.max_peerlist_size, 5);
 
 #if LIBTORRENT_VERSION_NUM >= 10102
     sp.set_int(sp.urlseed_max_request_bytes, 100 * 1024);
